102beb6a159931de832d9f5aaf3511078cbe807a
jadstrike <jadstrike@gmail.com>
2025-11-26 14:06:32 +0000

    updated code

--- visualization.R
+++ visualization.R
@@ -8,41 +8,30 @@ cwur_subset <- cwur[ , c("world_rank", "institution", "country",
                          "publications", "score", "year")]
 
 head(cwur_subset, 5)
+nrow(cwur)
 
 score_mean <- mean(cwur$score, na.rm = TRUE)
 score_sd   <- sd(cwur$score, na.rm = TRUE)
 
 plot(cwur$publications, cwur$score,
-     pch = 16,
-     col = "#00000025",
-     cex = 0.6,
      main = "Scatterplot of Score vs Publications",
      xlab = "Publications",
-     ylab = "Score",
-     frame.plot = TRUE)
-grid(col = "lightgray", lty = "dotted")
+     ylab = "Score")
 abline(lm(score ~ publications, data = cwur),
-       col = "red",
-       lwd = 3)
+       col = "red", lwd = 2)
 
 png("outputs/slide4_scatter_score_publications.png",
     width = 900, height = 650)
 plot(cwur$publications, cwur$score,
-     pch = 16,
-     col = "#00000025",
-     cex = 0.6,
      main = "Scatterplot of Score vs Publications",
      xlab = "Publications",
-     ylab = "Score",
-     frame.plot = TRUE)
-grid(col = "lightgray", lty = "dotted")
+     ylab = "Score")
 abline(lm(score ~ publications, data = cwur),
-       col = "red",
-       lwd = 3)
+       col = "red", lwd = 2)
 dev.off()
 
 hist(cwur$score,
-     breaks = 40,
+     breaks = 30,
      freq   = FALSE,
      main   = "Histogram of Score with Normal Curve",
      xlab   = "Score")
@@ -61,30 +50,20 @@ dev.off()
 
 set.seed(123)
 score_sample <- sample(cwur$score, 500)
-shapiro.test(score_sample)
+shapiro_result <- shapiro.test(score_sample)
 
 cor_test_spearman <- cor.test(cwur$score,
                               cwur$publications,
                               method = "spearman",
                               use    = "complete.obs")
 
-cor_test_pearson <- cor.test(cwur$score,
-                             cwur$publications,
-                             method = "pearson",
-                             use    = "complete.obs")
-
-sink("outputs/slide6_correlation_results.txt")
-print(shapiro.test(score_sample))
-print(cor_test_spearman)
-print(cor_test_pearson)
-sink()
-
 spearman_rho <- cor_test_spearman$estimate
 spearman_p   <- cor_test_spearman$p.value
-pearson_r    <- cor_test_pearson$estimate
-pearson_p    <- cor_test_pearson$p.value
 
 spearman_rho
 spearman_p
-pearson_r
-pearson_p
+
+sink("outputs/slide6_correlation_results.txt")
+print(shapiro_result)
+print(cor_test_spearman)
+sink()
